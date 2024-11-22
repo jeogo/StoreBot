afbe43889c2e82a3fa9bfdb0985e532f498702fc
jeogooussama <jeogodz@protonmail.com>
2024-11-22 22:02:27 +0100

    modified:   src/commands/support.ts

--- src/commands/support.ts
+++ src/commands/support.ts
@@ -2,16 +2,19 @@ import { Context, InlineKeyboard } from "grammy";
 
 export const handleSupportCommand = (ctx: Context) => {
   try {
-    // Create an inline keyboard with a WhatsApp link
-    const keyboard = new InlineKeyboard().url(
-      "تواصل عبر WhatsApp",
-      "https://wa.me/1234567890"
-    );
+    // Create an inline keyboard with WhatsApp and Telegram links
+    const keyboard = new InlineKeyboard()
+      .url("تواصل عبر WhatsApp", "https://wa.me/213557349101")
+      .row() // Add a new row for better organization
+      .url("تواصل عبر Telegram", "https://t.me/Shams0628");
 
     // Send the reply with the keyboard
-    ctx.reply("الدعم الفني لشحن الرصيد الرجاء التواصل معنا في الواتس اب ", {
-      reply_markup: keyboard,
-    });
+    ctx.reply(
+      "الدعم الفني لشحن الرصيد الرجاء التواصل معنا عبر الروابط التالية:",
+      {
+        reply_markup: keyboard,
+      }
+    );
   } catch (error) {
     console.error("Error in handleSupportCommand:", error);
     ctx.reply(
